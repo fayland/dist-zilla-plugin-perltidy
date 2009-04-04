93ba547f6b57d03061d0728598dfe79052fc767b
fayland <fayland@gmail.com>
2009-04-04 21:07:28 +0800

    more work

--- lib/Dist/Zilla/App/Command/perltidy.pm
+++ lib/Dist/Zilla/App/Command/perltidy.pm
@@ -4,22 +4,32 @@ use strict;
 use warnings;
 
 # ABSTRACT: perltidy your dist
-use Perl::Tidy;
 use Dist::Zilla::App -command;
 
 sub abstract {'perltidy your dist'}
 
 sub run {
-    my $self = shift;
-    my $opt  = shift;
-    my $arg  = shift;
+    my ( $self, $opt, $arg ) = @_;
 
+    my $zilla = $self->zilla;
+
+    # make Perl::Tidy happy
     local @ARGV = ();
-
+    
+    require Perl::Tidy;
     require File::Copy;
     require File::Next;
+    require File::HomeDir;
+    require File::Spec;
+    
+    my $config = { $self->config->flatten };
+    print STDERR Dumper(\$config);
+    
+    my $rcfile;
+    my $homerc = File::Spec->catfile( File::HomeDir->my_home, '.perltidyrc' );
+    $rcfile = $homerc if -e $homerc;
+    
     my $files = File::Next::files('.');
-
     while ( defined( my $file = $files->() ) ) {
         next unless ( $file =~ /\.(t|p[ml])$/ );    # perl file
         my $tidyfile = $file . '.tdy';
